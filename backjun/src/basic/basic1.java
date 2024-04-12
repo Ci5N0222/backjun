e818b60d70c5e609c3e7b14c65101b1b5e3fc1f3
SionNoh <vmfpsel@gmail.com>
2024-04-12 21:15:38 +0900

    start

--- backjun/src/basic/basic1.java
+++ backjun/src/basic/basic1.java
@@ -1,10 +1,71 @@
 package basic;
 
+import java.util.Scanner;
+
 public class basic1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		
+		/**
+		 *   세자리 수 끼리의 곱 (세자리 수) * (세자리 수)
+		 *   
+		 */
+		
+//		int num1 = sc.nextInt();
+//		int num2 = sc.nextInt();
+//		System.out.println(num1*(num2%10));
+//		System.out.println(num1*(num2%100/10));
+//		System.out.println(num1*(num2/100));
+//		System.out.println(num1*num2);
+		
+		
+		
+		/**
+		 *   (A+B)%C는 ((A%C) + (B%C))%C 와 같을까?
+		 *   (A×B)%C는 ((A%C) × (B%C))%C 와 같을까?
+		 *   세 수 A, B, C가 주어졌을 때, 위의 네 가지 값을 구하는 프로그램을 작성하시오.
+		 *   
+		 *   입력: 첫째 줄에 A, B, C가 순서대로 주어진다. (2 ≤ A, B, C ≤ 10000)
+		 *   출력: 첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
+		 *   예제 입력: 5 8 4
+		 *   예제 출력:
+		 *   		  1
+		 *   		  1
+		 *   		  0
+		 *   		  0
+		 *   
+		 */
+//		 int a = sc.nextInt();
+//		 int b = sc.nextInt();
+//		 int c = sc.nextInt();
+//		    
+//		 System.out.println((a+b)%c);
+//		 System.out.println(((a%c)+(b%c))%c);
+//		 System.out.println((a*b)%c);
+//		 System.out.println(((a%c)*(b%c))%c);
+		
+		
+		/**
+		 *  ICPC Bangkok Regional에 참가하기 위해 수완나품 국제공항에 막 도착한 팀 레드시프트 일행은 눈을 믿을 수 없었다. 
+		 *  공항의 대형 스크린에 올해가 2562년이라고 적혀 있던 것이었다.
+		 *  불교 국가인 태국은 불멸기원(佛滅紀元), 즉 석가모니가 열반한 해를 기준으로 연도를 세는 불기를 사용한다. 
+		 *  반면, 우리나라는 서기 연도를 사용하고 있다. 
+		 *  불기 연도가 주어질 때 이를 서기 연도로 바꿔 주는 프로그램을 작성하시오.
+		 *  
+		 *  입력: 서기 연도를 알아보고 싶은 불기 연도 y가 주어진다. (1000 ≤ y ≤ 3000)
+		 *  출력: 불기 연도를 서기 연도로 변환한 결과를 출력한다.
+		 *  예제 입력: 2541
+		 *  예제 출력: 1998
+		 *  
+		 */
+//		int year = sc.nextInt();
+//		System.out.println(year - 543);
+	
 	}
 
 }
